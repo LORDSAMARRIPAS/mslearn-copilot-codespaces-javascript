75dd80bebdbb14fb50839ae615edafdf369f7812
LORDSAMARRIPAS <rikepilb@student.ubc.ca>
2024-06-09 04:18:57 +0000

    update_siteProps&animate_icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Richard Pillaca",
+  title: "Web Designer & Developer",
+  email: "ridi.pillaca@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "richard-pillaca",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
