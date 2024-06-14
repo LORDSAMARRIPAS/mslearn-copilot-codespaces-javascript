d002da54801e4294e5cbbb8b9aded60ed59ec546
Richard Pillaca <rikepilb@student.ubc.ca>
2024-06-14 00:40:58 +0000

    Styles and components modifications

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Software Developer from The University of British Columbia. I love creating simplistic user interfaces and elegant solutions.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
